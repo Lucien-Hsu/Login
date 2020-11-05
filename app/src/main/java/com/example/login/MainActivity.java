259f0d4e0ab399930538f1435c90a3a13dec0ff3
Lucien-Hsu <meteor831@gmail.com>
2020-11-06 00:09:06 +0800

    feat:換頁、提示使用者未輸入資料 1.跳頁至登入頁、註冊頁 2.首頁資料未輸入時跳出提示
    issue #002

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
+    EditText etID, etPWD;
+    Button btnLogin, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+        etID = findViewById(R.id.et_id);
+        etPWD = findViewById(R.id.et_pwd);
+        btnLogin = findViewById(R.id.btn_login);
+        btnSignUp = findViewById(R.id.btn_sign_up);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(etID.length() == 0 || etPWD.length() == 0){
+                    Toast.makeText(context,"請輸入正確帳號密碼", Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
